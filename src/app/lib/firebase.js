95d30557e70ebd57c98c826eb8fc01dc024fd3d2
roob88257 <99383508+roob88257@users.noreply.github.com>
2023-10-20 20:31:06 +0530

    Update firebase.js

--- src/app/lib/firebase.js
+++ src/app/lib/firebase.js
@@ -9,7 +9,7 @@ import { toast } from "react-hot-toast";
 
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_AP,
+  apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
